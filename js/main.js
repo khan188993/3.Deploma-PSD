c59044fb0b760a7d90ac6f7faf74902b18f57ba0
ARFANKHAN <khan188993@gmail.com>
2020-10-09 01:49:46 +0600

    2.portfolio section add

--- js/main.js
+++ js/main.js
@@ -10,7 +10,24 @@ $(document).ready(function () {
 //       $(".mobile-nav").addClass("fade");
 //     });
 
-  
+  // Owl carousel Active code
+
+  $('.slider-active').owlCarousel({
+    loop:true,
+    nav:true,
+    navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
 
 
 
