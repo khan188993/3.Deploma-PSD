589f90660630aeffd7df724fc09868d99488e887
ARFANKHAN <khan188993@gmail.com>
2020-10-08 23:40:42 +0600

    1.all file add

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+  // mobile munu code 
+//  $(".hamberger").click(function () {
+//         $(".mobile-nav").addClass("open");
+//         $(".mobile-nav").removeClass("fade");
+//     });
+//     $(".times").click(function () {
+//       $(".mobile-nav").removeClass("open");
+//       $(".mobile-nav").addClass("fade");
+//     });
+
+  
+
+
+
+})
